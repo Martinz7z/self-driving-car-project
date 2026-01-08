fc02b63f824326b7ee31fbcc1ec1f27aca268d33
Martinz7z <80601115+Martinz7z@users.noreply.github.com>
2026-01-08 18:56:00 +0000

    Add basic data preprocessing skeleton

---
+++ src/data_preprocessing.py
@@ -0,0 +1,35 @@
+﻿"""
+Data Preprocessing Module
+Initial skeleton - will be expanded as we collect data
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+class DataLoader:
+    """Basic data loading functionality"""
+    
+    def __init__(self, data_path='data/training/'):
+        self.data_path = data_path
+        self.log_file = data_path + 'driving_log.csv'
+    
+    def check_data_exists(self):
+        """Check if training data is available"""
+        try:
+            if os.path.exists(self.log_file):
+                df = pd.read_csv(self.log_file)
+                return len(df)
+            return 0
+        except:
+            return 0
+    
+    def load_sample_data(self):
+        """Load a small sample for testing"""
+        # Will implement after data collection
+        print("Sample data loading - to be implemented")
+        return None
+
+if __name__ == "__main__":
+    loader = DataLoader()
+    print("Data loader initialized")
